954233cd2799fcfe6cf998eae89cd954a983a7ff
LouisCarpentier42 <louis.carpentier@kuleuven.be>
2023-09-27 13:38:23 +0200

    Added first test!

---
+++ tests/anomaly_detection/utility/test_Windowing.py
@@ -0,0 +1,16 @@
+
+import unittest
+import numpy as np
+
+from anomaly_detection.utility.Windowing import Windowing
+
+
+class TestWindowing(unittest.TestCase):
+
+    def test_windowing_window_size(self):
+        min_window_size = 20
+        max_window_size = 500
+        nb_reps = 50
+        for random_window_size in np.random.choice(np.arange(min_window_size, max_window_size + 1), nb_reps):
+            windowing = Windowing(window_size=random_window_size)
+            self.assertEquals(windowing.window_size, random_window_size)
