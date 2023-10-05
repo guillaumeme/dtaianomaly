9fd9b3f5f198dcce165034b7cdfffdf02b1548b8
LouisCarpentier42 <louis.carpentier@kuleuven.be>
2023-10-05 14:33:47 +0200

    Updated test to pytest

--- tests/anomaly_detection/utility/test_Windowing.py
+++ tests/anomaly_detection/utility/test_Windowing.py
@@ -1,16 +1,18 @@
 
-import unittest
 import numpy as np
 
 from src.anomaly_detection.utility.Windowing import Windowing
 
 
-class TestWindowing(unittest.TestCase):
+class TestWindowing:
 
-    def test_windowing_window_size(self):
-        min_window_size = 20
-        max_window_size = 500
-        nb_reps = 50
-        for random_window_size in np.random.choice(np.arange(min_window_size, max_window_size + 1), nb_reps):
+    min_window_size = 20
+    max_window_size = 500
+    nb_reps = 50
+
+    def test_get_window_size(self):
+        for random_window_size in np.random.choice(np.arange(self.min_window_size, self.max_window_size + 1), self.nb_reps):
             windowing = Windowing(window_size=random_window_size)
-            self.assertEqual(windowing.window_size, random_window_size)
+            assert windowing.window_size == random_window_size
+
+
--- tests/evaluation/test_thresholding.py
+++ tests/evaluation/test_thresholding.py
@@ -1,48 +1,47 @@
-import unittest
 import numpy as np
 
 from src.evaluation.thresholding import count_nb_ranges
 
 
-class TestCountConsecutiveOnes(unittest.TestCase):
+class TestCountConsecutiveOnes:
 
     def test_empty_list(self):
-        self.assertEqual(count_nb_ranges(np.array([])), 0)
+        assert count_nb_ranges(np.array([])) == 0
 
     def test_no_ones(self):
-        self.assertEqual(count_nb_ranges(np.array([0, 0, 0, 0, 0])), 0)
+        assert count_nb_ranges(np.array([0, 0, 0, 0, 0])) == 0
 
     def test_single_consecutive_ones(self):
-        self.assertEqual(count_nb_ranges(np.array([0, 1, 0, 0, 0, 0])), 1)
-        self.assertEqual(count_nb_ranges(np.array([0, 1, 0, 0, 1, 0, 0])), 2)
-        self.assertEqual(count_nb_ranges(np.array([0, 1, 0, 0, 1, 0, 0, 1])), 3)
+        assert count_nb_ranges(np.array([0, 1, 0, 0, 0, 0])) == 1
+        assert count_nb_ranges(np.array([0, 1, 0, 0, 1, 0, 0])) == 2
+        assert count_nb_ranges(np.array([0, 1, 0, 0, 1, 0, 0, 1])) == 3
 
     def test_multiple_consecutive_ones(self):
-        self.assertEqual(count_nb_ranges(np.array([0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 1, 0])), 3)
-        self.assertEqual(count_nb_ranges(np.array([0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 0])), 2)
-        self.assertEqual(count_nb_ranges(np.array([0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0])), 1)
+        assert count_nb_ranges(np.array([0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 1, 0])) == 3
+        assert count_nb_ranges(np.array([0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 0])) == 2
+        assert count_nb_ranges(np.array([0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0])) == 1
 
     def test_consecutive_ones_at_start(self):
-        self.assertEqual(count_nb_ranges(np.array([1, 1, 0, 0, 0, 1, 0])), 2)
-        self.assertEqual(count_nb_ranges(np.array([0, 1, 0, 0, 0, 1, 0])), 2)
-        self.assertEqual(count_nb_ranges(np.array([1, 0, 0, 0, 0, 1, 0])), 2)
-        self.assertEqual(count_nb_ranges(np.array([0, 0, 0, 0, 0, 1, 0])), 1)
+        assert count_nb_ranges(np.array([1, 1, 0, 0, 0, 1, 0])) == 2
+        assert count_nb_ranges(np.array([0, 1, 0, 0, 0, 1, 0])) == 2
+        assert count_nb_ranges(np.array([1, 0, 0, 0, 0, 1, 0])) == 2
+        assert count_nb_ranges(np.array([0, 0, 0, 0, 0, 1, 0])) == 1
 
-        self.assertEqual(count_nb_ranges(np.array([1, 1, 0, 0, 0, 0, 0])), 1)
-        self.assertEqual(count_nb_ranges(np.array([0, 1, 0, 0, 0, 0, 0])), 1)
-        self.assertEqual(count_nb_ranges(np.array([1, 0, 0, 0, 0, 0, 0])), 1)
-        self.assertEqual(count_nb_ranges(np.array([0, 0, 0, 0, 0, 0, 0])), 0)
+        assert count_nb_ranges(np.array([1, 1, 0, 0, 0, 0, 0])) == 1
+        assert count_nb_ranges(np.array([0, 1, 0, 0, 0, 0, 0])) == 1
+        assert count_nb_ranges(np.array([1, 0, 0, 0, 0, 0, 0])) == 1
+        assert count_nb_ranges(np.array([0, 0, 0, 0, 0, 0, 0])) == 0
 
     def test_consecutive_ones_at_end(self):
-        self.assertEqual(count_nb_ranges(np.array([0, 1, 1, 0, 0, 0, 1, 1])), 2)
-        self.assertEqual(count_nb_ranges(np.array([0, 1, 1, 0, 0, 0, 1, 0])), 2)
-        self.assertEqual(count_nb_ranges(np.array([0, 1, 1, 0, 0, 0, 0, 1])), 2)
-        self.assertEqual(count_nb_ranges(np.array([0, 1, 1, 0, 0, 0, 0, 0])), 1)
+        assert count_nb_ranges(np.array([0, 1, 1, 0, 0, 0, 1, 1])) == 2
+        assert count_nb_ranges(np.array([0, 1, 1, 0, 0, 0, 1, 0])) == 2
+        assert count_nb_ranges(np.array([0, 1, 1, 0, 0, 0, 0, 1])) == 2
+        assert count_nb_ranges(np.array([0, 1, 1, 0, 0, 0, 0, 0])) == 1
 
-        self.assertEqual(count_nb_ranges(np.array([0, 0, 0, 0, 0, 0, 1, 1])), 1)
-        self.assertEqual(count_nb_ranges(np.array([0, 0, 0, 0, 0, 0, 1, 0])), 1)
-        self.assertEqual(count_nb_ranges(np.array([0, 0, 0, 0, 0, 0, 0, 1])), 1)
-        self.assertEqual(count_nb_ranges(np.array([0, 0, 0, 0, 0, 0, 0, 0])), 0)
+        assert count_nb_ranges(np.array([0, 0, 0, 0, 0, 0, 1, 1])) == 1
+        assert count_nb_ranges(np.array([0, 0, 0, 0, 0, 0, 1, 0])) == 1
+        assert count_nb_ranges(np.array([0, 0, 0, 0, 0, 0, 0, 1])) == 1
+        assert count_nb_ranges(np.array([0, 0, 0, 0, 0, 0, 0, 0])) == 0
 
 
 
