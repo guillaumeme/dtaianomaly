41020b30c2a26cd78dd52d158b653cd74d61304b
Louis Carpentier <louis.carpentier@kuleuven.be>
2023-10-12 19:09:48 +0000

    Improve data manager

--- tests/anomaly_detection/__init__.py
+++
@@ -1,8 +0,0 @@
-
-# import pathlib
-# import sys
-# sys.path.append(str(pathlib.Path(__file__).parent.parent.parent.resolve()))
-# print(sys.path)
-#
-# # from anomaly_detection import *
-# print('REACHED')
