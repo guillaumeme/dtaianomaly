41017a978904640260930242081b179ca37ffb17
LouisCarpentier42 <louis.carpentier@kuleuven.be>
2023-10-24 10:15:23 +0200

    Update main.py and check python 3.12

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ from dtaianomaly.data_management import DataManager
 
 if __name__ == '__main__':
 
+    # Create a parser and parse the arguments
     parser = argparse.ArgumentParser(description="Time series anomaly detection.")
-
     parser.add_argument('--data_dir', default='data',
                         help='The directory containing the datasets the datasets and the index file for the '
                              'datasets.')
@@ -18,13 +18,11 @@ if __name__ == '__main__':
     parser.add_argument('--configuration_dir', default='configurations',
                         help='The directory containing the configuration files. The provided configurations '
                              'should be relative to this path.')
-
     parser.add_argument('--config',
                         help='The name of the configuration file for the experiment. This file'
                              'contains the paths to the configuration files for the data, the '
                              'algorithm, the metrics and the output. '
                              '(More information at ...)')
-
     parser.add_argument('--data',
                         help="The path of the configuration file regarding which data to read. Ignored \n"
                              "More information at ... Ignored if the '--config' parameter is given.")
@@ -37,27 +35,58 @@ if __name__ == '__main__':
     parser.add_argument('--output',
                         help="The path of the configuration file regarding the output of the workflow.\n"
                              "More information at ... Ignored if the '--config' parameter is given.")
-
     args = parser.parse_args()
 
+    # Variables for the different configurations
+    data_configuration = None
+    algorithm_configuration = None
+    metric_configuration = None
+    output_configuration = None
+
+    # Check if a single configuration file was given
     if args.config is not None:
-        configuration_file = open(args.configuration_dir + args.config, 'r')
+        configuration_file = open(args.configuration_dir + '/' + args.config, 'r')
         configuration = json.load(configuration_file)
         configuration_file.close()
-        data_configuration = configuration['data']
-        data_configuration = args.configuration_dir + '/' + data_configuration if type(data_configuration) is str else data_configuration
-        algorithm_configuration = configuration['algorithm']
-        algorithm_configuration = args.configuration_dir + '/' + algorithm_configuration if type(algorithm_configuration) is str else algorithm_configuration
-        metric_configuration = configuration['metric']
-        metric_configuration = args.configuration_dir + '/' + metric_configuration if type(metric_configuration) is str else metric_configuration
-        output_configuration = configuration['output']
-        output_configuration = args.configuration_dir + '/' + output_configuration if type(output_configuration) is str else output_configuration
 
-    else:
-        data_configuration = args.configuration_dir + '/' + args.data
-        algorithm_configuration = args.configuration_dir + '/' + args.algorithm
-        metric_configuration = args.configuration_dir + '/' + args.metric
-        output_configuration = args.configuration_dir + '/' + args.output
+        if 'data' in configuration:
+            data_configuration = configuration['data']
+        if 'algorithm' in configuration:
+            algorithm_configuration = configuration['algorithm']
+        if 'metric' in configuration:
+            metric_configuration = configuration['metric']
+        if 'output' in configuration:
+            output_configuration = configuration['output']
+
+    # Overwrite the configurations if specific values were given
+    if args.data is not None:
+        data_configuration = args.data
+    if args.algorithm is not None:
+        algorithm_configuration = args.algorithm
+    if args.metric is not None:
+        metric_configuration = args.metric
+    if args.output is not None:
+        output_configuration = args.output
+
+    # Check if the configurations were given
+    if data_configuration is None:
+        raise ValueError("No data configuration was given!")
+    if algorithm_configuration is None:
+        raise ValueError("No algorithm configuration was given!")
+    if metric_configuration is None:
+        raise ValueError("No metric configuration was given!")
+    if output_configuration is None:
+        raise ValueError("No output configuration was given!")
+
+    # Set the path correctly, if the configuration is a string (aka a path to a .json file)
+    if type(data_configuration) is str:
+        data_configuration = args.configuration_dir + '/' + data_configuration
+    if type(algorithm_configuration) is str:
+        algorithm_configuration = args.configuration_dir + '/' + algorithm_configuration
+    if type(metric_configuration) is str:
+        metric_configuration = args.configuration_dir + '/' + metric_configuration
+    if type(output_configuration) is str:
+        output_configuration = args.configuration_dir + '/' + output_configuration
 
     # Execute the algorithm
     execute_algorithm(
