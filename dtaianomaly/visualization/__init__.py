d133b406632bbf485e3276d900d478ef674efe71
Louis Carpentier <louis.carpentier@kuleuven.be>
2024-01-25 14:27:10 +0000

    update visualization

--- dtaianomaly/visualization/__init__.py
+++ dtaianomaly/visualization/__init__.py
@@ -1,6 +1,11 @@
 
 """
-The visualization documentation.
+This module contains the all functionality regarding visualizing the data. It can be imported as follows:
+
+.. code-block:: python
+
+   from dtaianomaly import visualization
 """
 
-from .plot_anomalies import plot_data, plot_anomaly_scores
+from .plot_data import plot_data, plot_anomaly_scores
+from .plot_results import read_results, plot_performance, plot_ranking, plot_critical_difference_diagram
